74070ad484d4974614b09ab3188cf817850f701b
Francesco <71893290+FrancescoCode1@users.noreply.github.com>
2021-03-17 21:23:10 +0100

    Update bot.py
    Changed the way the request is done. Fixed content type error. fixed count error. added ready indicator

--- bot.py
+++ bot.py
@@ -18,16 +18,15 @@ client = commands.Bot(command_prefix='!', intents=intents)
 #client.load_extension("cogs.repz_bf3")
 
 #as the bot goes online, status of bot changes to server stats.
-#ToDo: iterate through the method every 2 seconds so it updates, json request does not work yet. 
+#ToDo: iterate through the method every 2 seconds so it updates.
 @client.event
 async def on_ready():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://bf3.zloemu.net/servers?id=31197&json') as r:
-            if r.status == 200:
-                js = await r.json()
-                await client.change_presence(status=discord.Status.idle, activity=discord.Game(js['name']))
-            else:
-                await ctx.send("This aint supposed to happen")
+            js = await r.json(content_type='text/html')
+    count = len(js['players'])
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game(str(count) + " / 64 Players"))
+    print("Ready")
 
 
 #store your token in a .env file. Never hardcode tokens into source
