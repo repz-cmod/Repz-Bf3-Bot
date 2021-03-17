4d53096a8038653d19ac8726f0eea9d5e51fa9b1
Francesco <71893290+FrancescoCode1@users.noreply.github.com>
2021-03-17 21:41:40 +0100

    Update repz_bf3.py

--- cogs/repz_bf3.py
+++ cogs/repz_bf3.py
@@ -1,16 +1,12 @@
 import discord
-import os
-import random
-import glob
 from discord.ext import commands
-import datetime
 import aiohttp
 
-
+#base class all cogs inherit from
 class repz_bf3(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+    #sends list of players in chat. sends the raw list so it needs formatting
     @commands.command(name="players")
     async def on_players(self, ctx):
         async with aiohttp.ClientSession() as session:
@@ -18,6 +14,6 @@ class repz_bf3(commands.Cog):
                 js = await r.json(content_type='text/html')
         await ctx.send(js['players'])
 
+        
 def setup(bot):
     bot.add_cog(repz_bf3(bot))
-
